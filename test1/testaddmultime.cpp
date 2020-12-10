b68798dac05acd11f4396ea303b91b7e0110c543
AzureZzz <572768692@qq.com>
2020-12-10 22:32:22 +0800

    update

--- test1/testaddmultime.cpp
+++ test1/testaddmultime.cpp
@@ -5,37 +5,51 @@
 #include <time.h>
 using namespace std;
 
-	double getUsedTimeMs(LONGLONG start, LONGLONG end, double dfFreq)
+double getUsedTimeMs(LONGLONG start, LONGLONG end, double dfFreq)
+{
+	return (double)(end - start) * 1000 / dfFreq;
+}
+//Windows
+void test(int repeat = 1)
+{
+	LARGE_INTEGER litmp;
+	LONGLONG start, end;
+	int a = rand();
+	int b = rand();
+	int c;
+	double dfFreq;
+	QueryPerformanceFrequency(&litmp);
+	dfFreq = (double)litmp.QuadPart;
+	double sum = 0.0;
+	QueryPerformanceCounter(&litmp);
+	start = litmp.QuadPart;
+	while (repeat--)
 	{
-		return (double)(end - start) * 1000 / dfFreq;
+		a + b;
 	}
-	//Windows
-	void test(int repeat = 1)
-	{
-		LARGE_INTEGER litmp;
-		LONGLONG start, end;
-        int a = rand();
-        int b = rand();
-        int c;
-		double dfFreq;
-		QueryPerformanceFrequency(&litmp);
-		dfFreq = (double)litmp.QuadPart;
-		double sum = 0.0;
-		QueryPerformanceCounter(&litmp);
-		start = litmp.QuadPart;
-		while (repeat--)
-		{
-			a+b;
-        }
-		QueryPerformanceCounter(&litmp);
-		end = litmp.QuadPart;
-		cout << getUsedTimeMs(start,end,dfFreq) << "ms" << endl;
-	}
-
-
+	QueryPerformanceCounter(&litmp);
+	end = litmp.QuadPart;
+	cout << getUsedTimeMs(start, end, dfFreq) << "ms" << endl;
+}
 
 int main()
 {
 	test(100000000);
-	return 0;
+	// return 0;
+
+	LARGE_INTEGER litmp;
+	LONGLONG start, end;
+	double dfFreq;
+	QueryPerformanceFrequency(&litmp);
+	dfFreq = (double)litmp.QuadPart;
+	
+	QueryPerformanceCounter(&litmp);
+	start = litmp.QuadPart;
+
+	//todo
+
+	QueryPerformanceCounter(&litmp);
+	end = litmp.QuadPart;
+
+	cout << ((double)(end - start) * 1000 / dfFreq) << "ms" << endl;
 }
